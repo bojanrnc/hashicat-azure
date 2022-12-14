59c6dc6afe7eb2b59ae296d40942f320016e4a6f
Bojan Rakonjac <bojan.rakonjac@nordcloud.com>
2022-12-14 13:06:56 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/bojanr-training-tf/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "gaurav-vnet-rg"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
